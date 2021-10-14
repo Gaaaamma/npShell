b069aae56fb8c41d61fb6e077f46402231410a22
yangtc552022 <yangtc552022@cs.nctu.edu.tw>
2021-10-14 15:07:32 +0800

    inputParsing done

--- npshell.cpp
+++ npshell.cpp
@@ -1,9 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <cstdlib>
+#define MAX_LENGTH 15000
 using namespace std;
 
-int main(int argc,char* const argv[]){
+void callPrintenv(string envVar);
+void callSetenv(string envVar,string value);
 
+int main(int argc, char *argv[]) {
+  string input ="" ; 
+  string aWord ="" ;
+  stringstream ss;
+  vector<string> commandVec;
+  
+  // set $PATH to bin/ ./ initially
+  callSetenv("PATH","bin:.");
+  callPrintenv("PATH");
+ 
+  while(getline(cin,input)){
+    ss << input ;
+    while (ss >> aWord) {
+      commandVec.push_back(aWord);
+    }
+    // Ready to handle the command.
+    
+    // ...
+
+    //one term command is done -> Initialize it.
+    commandVec.clear();
+    ss.str("");
+    ss.clear();
+  }
+
+  return 0;
 }
 
+// three built-in commands(setenv,printenv,exit)
+void callPrintenv(string envVar){
+  const char* input = envVar.c_str() ;
+  char* path_string = getenv (input);
+
+  if (path_string!=NULL)
+    cout <<path_string<<endl;
+}
+
+void callSetenv(string envVar,string value){
+  const char *envname = envVar.c_str();
+  const char *envval = value.c_str();
+  setenv(envname,envval,1);
+}
