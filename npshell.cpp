9b7ab49f5d6a2f84fbb9a9ebb82721efd701bd1a
yangtc552022 <yangtc552022@cs.nctu.edu.tw>
2021-10-20 23:44:39 +0800

    DONE -> need to debug input 5.txt

--- npshell.cpp
+++ npshell.cpp
@@ -20,7 +20,7 @@ using namespace std;
 void callPrintenv(string envVar);
 void callSetenv(string envVar,string value);
 void singleProcess(vector<string>commandVec,bool hasNumberPipe,bool bothStderr,int pipeAfterLine,int numberPipe[PET_SIZE][2],int pipe_expired_table[PET_SIZE]);
-void multiProcess(vector<string>commandVec,int process_count);
+void multiProcess(vector<string>commandVec,int process_count,bool hasNumberPipe,bool bothStderr,int pipeAfterLine,int numberPipe[PET_SIZE][2],int pipe_expired_table[PET_SIZE]);
 
 void PET_init(int pipe_expired_table[PET_SIZE]);
 void PET_iterate(int pipe_expired_table[PET_SIZE]); 
@@ -28,6 +28,7 @@ int PET_findExpired(int pipe_expired_table[PET_SIZE]);
 int PET_findSameLine(int pipe_expired_table[PET_SIZE],int target_line);
 int PET_emptyPipeIndex(int pipe_expired_table[PET_SIZE]);
 vector<int> PET_existPipe(int pipe_expired_table[PET_SIZE]);
+void wait4children(int signo);
 
 int main(int argc, char *argv[]) {
 	string input ="" ; 
@@ -105,7 +106,7 @@ int main(int argc, char *argv[]) {
 			if(process_count ==1){
 				singleProcess(commandVec,hasNumberPipe,bothStderr,pipeAfterLine,numberPipe,pipe_expired_table);	
 			}else if(process_count>=2){
-				multiProcess(commandVec,process_count);				
+				multiProcess(commandVec,process_count,hasNumberPipe,bothStderr,pipeAfterLine,numberPipe,pipe_expired_table);	
 			}		
 		}
 
@@ -230,15 +231,16 @@ void singleProcess(vector<string> commandVec,bool hasNumberPipe,bool bothStderr,
 		// Parent doesn't need to wait child DONE.
 		// otherwise need to wait.
 		if(hasNumberPipe == true){
-			signal(SIGCHLD,SIG_IGN);
+			signal(SIGCHLD,wait4children);
 		}else{
 			waitpid(fork_pid,NULL,0);
 		}
 	}
 
 }
+
 // Multi process handle
-void multiProcess(vector<string> commandVec,int process_count){
+void multiProcess(vector<string>commandVec,int process_count,bool hasNumberPipe,bool bothStderr,int pipeAfterLine,int numberPipe[PET_SIZE][2],int pipe_expired_table[PET_SIZE]){
 	// First handle all of the command and it's argument
 	int process_index =0;
 	int process_cmd_index=0;
@@ -249,6 +251,12 @@ void multiProcess(vector<string> commandVec,int process_count){
 	string redirectionFileName = "" ;
 	char* arg[process_count][256]={NULL} ;
 	
+	// If number pipe expired -> remember to handle it.
+	int newNumberPipeIndex ;
+	int pipeToSameLine ;
+	int expiredIndex = PET_findExpired(pipe_expired_table);	
+	vector<int> existPipeIndex = PET_existPipe(pipe_expired_table); 
+	
 	// Handle argument and file redirection setting.
 	for(int cmd_index=0; cmd_index<commandVec.size() ; cmd_index++){
 		if( commandVec[cmd_index].find("|")!= string::npos){ //Find | in this string
@@ -256,8 +264,6 @@ void multiProcess(vector<string> commandVec,int process_count){
 				// single "|"
 				process_index ++;
 				process_cmd_index =0 ;
-			}else{
-				// Number pipe.
 			}
 		}else if(commandVec[cmd_index].find(">")!= string::npos){ //Find > in this string
 			if((cmd_index+1) < commandVec.size()){ 
@@ -268,27 +274,44 @@ void multiProcess(vector<string> commandVec,int process_count){
 				cerr << "syntax error near unexpected token\n";
 				return ;
 			}		
+		}else if(commandVec[cmd_index].find("!")!= string::npos){ //Find ! in this string
+			break ;
 		}else{
 			arg[process_index][process_cmd_index] = strdup(commandVec[cmd_index].c_str());
 			process_cmd_index ++ ;
 		}		
 	}
+
 	// All argument of process is handled.
 	// Now we are going to create the pipe.
 	int mPipe[2][2];
 	process_index =0 ;
 
 	if(process_count ==2){
-		pipe(mPipe[0]);
-		
+		pipe(mPipe[0]);	
+
 		if((fork_pid[0]=fork()) == -1){
 			cout <<"Multiprocess(...)-process_count=2 : fork error\n";
 		}else if(fork_pid[0] ==0){ //child1
+			//if there is number pipe expired
+			if(expiredIndex != -1){
+				// set pipe to STDIN and close the pipe
+				close(numberPipe[expiredIndex][1]);
+				dup2(numberPipe[expiredIndex][0],STDIN_FILENO);
+				close(numberPipe[expiredIndex][0]);
+			}		
+
 			// Child1 need to modify the stdout
 			close(mPipe[0][0]);
 			dup2(mPipe[0][1],STDOUT_FILENO);
 			close(mPipe[0][1]);
-		
+			
+			// Before execvp -> close useless number Pipe
+			for(int i=0;i<existPipeIndex.size();i++){
+				close(numberPipe[existPipeIndex[i]][0]);
+				close(numberPipe[existPipeIndex[i]][1]);
+			}
+
 			// Ready to execvp
 			if(execvp(arg[0][0],arg[0]) == -1){ // execvp fail (maybe bug)
 				cerr <<"Unknown command: ["<<arg[0][0]<<"].\n";
@@ -296,6 +319,24 @@ void multiProcess(vector<string> commandVec,int process_count){
 			}
 
 		}else if(fork_pid[0] >0) { //Parent
+			// Parent need to tidy expired number pipe
+			if(expiredIndex != -1){
+				close(numberPipe[expiredIndex][0]);
+				close(numberPipe[expiredIndex][1]);
+			}
+
+			// If this command need to number pipe
+			// First check if there is any pipe want to write to the same line
+			// If No -> create the pipe and set expired_table
+			if(hasNumberPipe == true){
+				pipeToSameLine = PET_findSameLine(pipe_expired_table,pipeAfterLine);
+				if(pipeToSameLine == -1){ // there isn't any  pipe want to write to the same line.	
+					newNumberPipeIndex = PET_emptyPipeIndex(pipe_expired_table);
+					pipe(numberPipe[newNumberPipeIndex]);
+					pipe_expired_table[newNumberPipeIndex] = pipeAfterLine ;
+				}
+			}
+
 			if( (fork_pid[1]=fork()) ==-1){
 				cout <<"Multiprocess(...)-process_count=2 : fork error2\n";
 			}else if(fork_pid[1] ==0){ //child2
@@ -303,12 +344,37 @@ void multiProcess(vector<string> commandVec,int process_count){
 				dup2(mPipe[0][0],STDIN_FILENO);
 				close(mPipe[0][0]);
 
+				//if this child need to number pipe to another line
+				if(hasNumberPipe==true){
+					if(pipeToSameLine == -1){
+						// Use the new Pipe.
+						close(numberPipe[newNumberPipeIndex][0]); //close read
+						dup2(numberPipe[newNumberPipeIndex][1],STDOUT_FILENO); //dup write to STDOUT_FILENO
+						if(bothStderr == true){
+							dup2(numberPipe[newNumberPipeIndex][1],STDERR_FILENO);
+						}
+						close(numberPipe[newNumberPipeIndex][1]); //close write
+					}else{
+						// Write to the old pipe.
+						dup2(numberPipe[pipeToSameLine][1],STDOUT_FILENO);
+						if(bothStderr == true ){
+							dup2(numberPipe[pipeToSameLine][1],STDERR_FILENO);
+						}
+					}
+				}
+
 				if(needRedirection){
      				int fd = open(redirectionFileName.c_str(),O_WRONLY|O_CREAT|O_TRUNC,S_IRUSR|S_IWUSR);
      				dup2(fd,STDOUT_FILENO);
      				close(fd);
 				}
 				
+				// Before execvp -> close useless number pipe
+				for(int i=0;i<existPipeIndex.size();i++){
+					close(numberPipe[existPipeIndex[i]][0]);
+					close(numberPipe[existPipeIndex[i]][1]);
+				}
+
 				//Ready to execvp
 				if(execvp(arg[1][0],arg[1]) == -1){
 					cerr <<"Unknown command: ["<<arg[1][0]<<"].\n";
@@ -317,9 +383,15 @@ void multiProcess(vector<string> commandVec,int process_count){
 
 			}else if(fork_pid[1] >0){ //parent
 				close(mPipe[0][0]);
-				close(mPipe[0][1]);
-				wait(NULL);
-				wait(NULL);
+				close(mPipe[0][1]);				
+
+				if(hasNumberPipe == true){
+					// Needless to wait the process
+					signal(SIGCHLD,wait4children);
+				}else{
+					wait(NULL);
+					wait(NULL);
+				}
 			} 	
 		}
 
@@ -330,11 +402,25 @@ void multiProcess(vector<string> commandVec,int process_count){
 		if((fork_pid[0]=fork()) == -1){
 			cout <<"Multiprocess(...)-process_count>2 : fork error\n";
 		}else if(fork_pid[0] ==0){ //child1
+			//if there is number pipe expired
+			if(expiredIndex != -1){
+				// set pipe to STDIN and close the pipe
+				close(numberPipe[expiredIndex][1]);
+				dup2(numberPipe[expiredIndex][0],STDIN_FILENO);
+				close(numberPipe[expiredIndex][0]);
+			}
+
 			// Child1 need to modify the stdout
 			close(mPipe[0][0]);
 			dup2(mPipe[0][1],STDOUT_FILENO);
 			close(mPipe[0][1]);
 		
+			// Before execvp -> close useless number Pipe
+			for(int i=0;i<existPipeIndex.size();i++){
+				close(numberPipe[existPipeIndex[i]][0]);
+				close(numberPipe[existPipeIndex[i]][1]);
+			}
+
 			// Ready to execvp
 			if(execvp(arg[process_index][0],arg[process_index]) == -1){
 				cerr <<"Unknown command: ["<<arg[process_index][0]<<"].\n";
@@ -344,14 +430,20 @@ void multiProcess(vector<string> commandVec,int process_count){
 		}else if(fork_pid[0] >0) { //Parent
 			process_index++;
 			// need to regist signal for the first child
-			signal(SIGCHLD,SIG_IGN);
+			signal(SIGCHLD,wait4children);
+
+			// Parent need to tidy expired number pipe
+			if(expiredIndex != -1){
+				close(numberPipe[expiredIndex][0]);
+				close(numberPipe[expiredIndex][1]);
+			}
 
 			// Handle Process 2,3,4,5,....,n-1 in the middle.
 			for(int i=1;i<process_count-1;i++){
 				pipe(mPipe[process_index%2]);
 
 				if( (fork_pid[process_index%2]=fork()) ==-1){
-					cout <<"Multiprocess-process_count>2: fork error:"<<process_index<<"\n";
+					// might handle it
 				}else if(fork_pid[process_index%2] ==0){ //child
 					close(mPipe[(process_index-1)%2][1]); //close front write.
 					dup2(mPipe[(process_index-1)%2][0],STDIN_FILENO ); //dup front read to STDIN
@@ -361,6 +453,12 @@ void multiProcess(vector<string> commandVec,int process_count){
 					dup2(mPipe[process_index%2][1],STDOUT_FILENO); //dup behind write to STDOUT
 					close(mPipe[process_index%2][1]); //close behind write
 					
+					// Before execvp -> close useless number pipe
+					for(int i=0;i<existPipeIndex.size();i++){
+						close(numberPipe[existPipeIndex[i]][0]);
+						close(numberPipe[existPipeIndex[i]][1]);
+					}
+
 					// Ready to execvp
 					if(execvp(arg[process_index][0],arg[process_index])==-1){
 						cerr <<"Unknown command: ["<<arg[process_index][0] <<"].\n";
@@ -373,16 +471,48 @@ void multiProcess(vector<string> commandVec,int process_count){
 					close(mPipe[(process_index-1)%2][1]);
 					
 					// Maybe need to regist signal for each child in the middle.
-					signal(SIGCHLD,SIG_IGN);
+					signal(SIGCHLD,wait4children);
 				}
 				process_index++ ;
 			}
 			
 			// Handle Process n which is the last one in process.
 			pid_t last_process ;
+			
+			// If this command need to number pipe
+			// First check if there is any pipe want to write to the same line
+			// If No -> create the pipe and set expired_table
+			if(hasNumberPipe == true){
+				pipeToSameLine = PET_findSameLine(pipe_expired_table,pipeAfterLine);
+				if(pipeToSameLine == -1){ // there isn't any  pipe want to write to the same line.	
+					newNumberPipeIndex = PET_emptyPipeIndex(pipe_expired_table);
+					pipe(numberPipe[newNumberPipeIndex]);
+					pipe_expired_table[newNumberPipeIndex] = pipeAfterLine ;
+				}
+			}
+
 			if((last_process=fork())==-1){ //
 				cout <<"last process fork error:"<<process_index<<"\n";
 			}else if(last_process ==0){ //child
+				//if this child need to number pipe to another line
+				if(hasNumberPipe==true){
+					if(pipeToSameLine == -1){
+						// Use the new Pipe.
+						close(numberPipe[newNumberPipeIndex][0]); //close read
+						dup2(numberPipe[newNumberPipeIndex][1],STDOUT_FILENO); //dup write to STDOUT_FILENO
+						if(bothStderr == true){
+							dup2(numberPipe[newNumberPipeIndex][1],STDERR_FILENO);
+						}
+						close(numberPipe[newNumberPipeIndex][1]); //close write
+					}else{
+						// Write to the old pipe.
+						dup2(numberPipe[pipeToSameLine][1],STDOUT_FILENO);
+						if(bothStderr == true ){
+							dup2(numberPipe[pipeToSameLine][1],STDERR_FILENO);
+						}
+					}
+				}
+			
 				close(mPipe[(process_index-1)%2][1]); //close front write
 				dup2(mPipe[(process_index-1)%2][0],STDIN_FILENO); //dup front read to STDIN
 				close(mPipe[(process_index-1)%2][0]); // close front read
@@ -391,6 +521,12 @@ void multiProcess(vector<string> commandVec,int process_count){
      				int fd = open(redirectionFileName.c_str(),O_WRONLY|O_CREAT|O_TRUNC,S_IRUSR|S_IWUSR);
      				dup2(fd,STDOUT_FILENO);
      				close(fd);
+				}
+
+				// Before execvp -> close useless number pipe
+				for(int i=0;i<existPipeIndex.size();i++){
+					close(numberPipe[existPipeIndex[i]][0]);
+					close(numberPipe[existPipeIndex[i]][1]);
 				}
 
 				//Ready to execvp
@@ -405,7 +541,13 @@ void multiProcess(vector<string> commandVec,int process_count){
 				close(mPipe[(process_index-1)%2][1]);
 				// Last process we can use wait -> SHELL will stop here
 				// Until the last process done
-				waitpid(last_process,NULL,0);		
+				if(hasNumberPipe ==true){
+					// needless to wait
+					signal(SIGCHLD,wait4children);
+				}else{
+					// wait the last process
+					waitpid(last_process,NULL,0);
+				}
 			}
 		}
 	}			
@@ -484,4 +626,10 @@ void callSetenv(string envVar,string value){
 	const char *envname = envVar.c_str();
 	const char *envval = value.c_str();
 	setenv(envname,envval,1);
+}
+
+// signal handler
+void wait4children(int signo){
+	int status;
+	while(waitpid(-1,&status,WNOHANG)>0);
 }
